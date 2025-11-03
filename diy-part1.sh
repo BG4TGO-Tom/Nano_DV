b938252e2145dd0d860883a4097ecb364e81e3fe
BG4TGO-Tom <bg4tgo@126.com>
2025-11-03 14:18:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,3 +20,7 @@ sed -i '$a src-git immortalwrt https://github.com/immortalwrt/packages' feeds.co
 sed -i '$a src-git mmdvm https://github.com/qzyuebing/mmdvm-openwrt' feeds.conf.default
 #sed -i '$a src-git mmdvm https://github.com/lazywalker/mmdvm-openwrt' feeds.conf.default
 sed -i '$a src-git devtools https://github.com/lazywalker/devtools-feeds' feeds.conf.default
+
+echo "src-git helloworld https://github.com/fw876/helloworld" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages" >> feeds.conf.default
+echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
